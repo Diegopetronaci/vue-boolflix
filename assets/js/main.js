ec214ace18ae0f97e4474fbed6532f5b733d26b8
Diego <Petronacidiego@gmail.com>
2021-01-12 16:02:17 +0100

    primo ostacolo superato

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,13 +11,28 @@ let app = new Vue({
     methods: {
         inviaRichiesta() {
             
-            axios
-                .get('https://api.themoviedb.org/3/movie/550?api_key=d0d87b85e549264b7f1c08605be5929d')
+            /* axios
+                .get('https://api.themoviedb.org/3/search/movie?api_key=d0d87b85e549264b7f1c08605be5929d&language=en-US&query=matrix&page=1&include_adult=false')
                 .then(response => {
                     this.films = response.data;
                     console.log(response.data);
+                })  */
+
+            const self = this;
+            axios
+                .get("https://api.themoviedb.org/3/search/movie", {
+                    params: {
+                        api_key: "d0d87b85e549264b7f1c08605be5929d",
+                        query: self.search,
+                        language: "it-IT"
+                    }
                 })
-        }
+                .then(response => {
+                    /* this.films  */
+                    const risultato = response.data.results;
+                    console.log(risultato);
+                })
+        } 
     },
     /* computed: {
         listaFiltrata() {
