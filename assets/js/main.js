d2a421050c8eca0c929ca0400ad41d2607492649
Diego <Petronacidiego@gmail.com>
2021-01-12 17:49:07 +0100

    inizio millestone 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,13 +10,13 @@ let app = new Vue({
 
     methods: {
         inviaRichiesta() {
-            
-            /* axios
-                .get('https://api.themoviedb.org/3/search/movie?api_key=d0d87b85e549264b7f1c08605be5929d&language=en-US&query=matrix&page=1&include_adult=false')
+            /* const self = this;
+            axios
+                .get(`'https://api.themoviedb.org/3/search/movie?api_key=d0d87b85e549264b7f1c08605be5929d&language=en-US&query=${self.search}&page=1&include_adult=false'`)
                 .then(response => {
                     this.films = response.data;
                     console.log(response.data);
-                })  */
+                }) */
 
             const self = this;
             axios
@@ -28,30 +28,23 @@ let app = new Vue({
                     }
                 })
                 .then(response => {
-                    /* this.films  */
                     const risultato = response.data.results;
+                    this.films = risultato
                     console.log(risultato);
-                })
-        } 
-    },
-    /* computed: {
-        listaFiltrata() {
-            return this.films.filter(element => {
-                return element.data.title.toLowerCase().includes(this.search.toLowerCase())
-            })
-        }
-    }, */
-    /* mounted() {
-        for (let i = 0; i < 10; i++) {
 
-            axios
-            .get('https://api.themoviedb.org/3/movie/550?api_key=d0d87b85e549264b7f1c08605be5929d')
-            .then(response => {
-                this.films = response.data.response;
-                console.log(response);
-            })
-        }
-    }, */
+                    for (let i = 0; i < self.films.length; i++) {
+                        const votoDaTrasformare = Math.ceil(response.data.results[i].vote_average / 2);
+                        console.log(votoDaTrasformare);
+                    }
+                })
+        },
+        /* trasformaVoto(){
+            for (let i = 0; i < self.films.length; i++) {
+                const votoDaTrasformare = response.data.results[i].vote_average / 2
+                console.log(votoDaTrasformare);  
+            }
+        } */
+    },
 })
 
 
