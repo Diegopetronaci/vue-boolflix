74d95fa24ce7e68c25114b9057ad9d9bfe20eae9
Diego <Petronacidiego@gmail.com>
2021-01-13 17:29:25 +0100

    commit millestone 2 completato

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,12 +6,11 @@ let app = new Vue({
     data: {
         search: "",
         films: [],
-        arrayVoto: [],
-        stelle: null,
+        tvSeries: [],
     },
 
     methods: {
-        inviaRichiesta() {
+        inviaRichiestaFilm() {
             /* const self = this;
             axios
                 .get(`'https://api.themoviedb.org/3/search/movie?api_key=d0d87b85e549264b7f1c08605be5929d&language=en-US&query=${self.search}&page=1&include_adult=false'`)
@@ -26,7 +25,7 @@ let app = new Vue({
                     params: {
                         api_key: "d0d87b85e549264b7f1c08605be5929d",
                         query: self.search,
-                        language: "it-IT"
+                        language: "it"
                     }
                 })
                 .then(response => {
@@ -34,18 +33,40 @@ let app = new Vue({
                     this.films = risultato
                     console.log(risultato);
 
-                    for (let i = 0; i < self.films; i++) {
+                    /* let n = 5 ;
+                    for (let i = 0; i < n ; i++) {
+                        self.arrayVoto.push(self.voto)
+                        console.log(self.arrayVoto);
+                    } */
+
+                    this.films.forEach(element => {
+                        
                         /* const rispostaBase = response.data.results; */
-                        const votoDaTrasformare = Math.ceil(response.data.results[i].vote_average / 2);
+                        const votoDaTrasformare = Math.ceil(element.vote_average / 2);
+                        return element.stelle = votoDaTrasformare;
                         /* console.log(votoDaTrasformare); */
-                        response.data.results[i].vote_average = votoDaTrasformare;
+                        //response.data.results[i].vote_average = votoDaTrasformare;
                         /* console.log(response.data.results[i].vote_average); */
-                        const votoTrasformato = votoDaTrasformare;
-                        this.stelle = votoTrasformato;
+                        //const votoTrasformato = votoDaTrasformare;
+                        //this.stelle = votoTrasformato;
                         //console.log(votoDaTrasformare);
                         /* const votazioneTrasformata = rispostaBase[i].vote_average
-                        console.log(votazioneTrasformata); */ 
+                        console.log(votazioneTrasformata); */
+                    
+                    });
+                })
+            axios
+                .get("https://api.themoviedb.org/3/search/tv", {
+                    params: {
+                        api_key: "d0d87b85e549264b7f1c08605be5929d",
+                        query: self.search,
+                        language: "it"
                     }
+                })
+                .then(response => {
+                    const risultato = response.data.results;
+                    this.tvSeries = risultato
+                    console.log(risultato);
 
                     /* let n = 5 ;
                     for (let i = 0; i < n ; i++) {
@@ -53,14 +74,23 @@ let app = new Vue({
                         console.log(self.arrayVoto);
                     } */
 
+                    this.tvSeries.forEach(element => {
+
+                        /* const rispostaBase = response.data.results; */
+                        const votoDaTrasformare = Math.ceil(element.vote_average / 2);
+                        return element.stelle = votoDaTrasformare;
+                        /* console.log(votoDaTrasformare); */
+                        //response.data.results[i].vote_average = votoDaTrasformare;
+                        /* console.log(response.data.results[i].vote_average); */
+                        //const votoTrasformato = votoDaTrasformare;
+                        //this.stelle = votoTrasformato;
+                        //console.log(votoDaTrasformare);
+                        /* const votazioneTrasformata = rispostaBase[i].vote_average
+                        console.log(votazioneTrasformata); */
+
+                    });
                 })
         },
-        /* trasformaVoto(){
-            for (let i = 0; i < self.films.length; i++) {
-                const votoDaTrasformare = response.data.results[i].vote_average / 2
-                console.log(votoDaTrasformare);  
-            }
-        } */
     },
 })
 
